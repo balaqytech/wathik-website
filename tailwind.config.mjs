7205d5874bd381b390b210b82326b2d109535261
As3ad Altabali <as3ad.moh@gmail.com>
2024-06-12 14:31:24 +0300

    build wathik website

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	
 	theme: {
-		extend: {},
+		extend: {			
+			fontFamily:{
+				'sans': ['GESS', 'ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'],
+			},
+			colors:{
+				'primary': '#478CDB',
+				'accent': '#01BACE'
+			}
+		},
 	},
 	plugins: [],
 }
